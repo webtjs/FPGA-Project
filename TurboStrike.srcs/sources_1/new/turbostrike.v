bf14915b811f927f77cba2222dd8ef98d5ddad37
webster <webster.tan01@gmail.com>
2024-03-28 23:19:27 +0800

    Initialise project base

---
+++ TurboStrike.srcs/sources_1/new/turbostrike.v
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 28.03.2024 23:14:45
+// Design Name: 
+// Module Name: turbostrike
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module turbostrike();
+endmodule
